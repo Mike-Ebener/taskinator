75984675bf0e7287aa78a3fb81fb7ab7638fbe77
ME <mebener44@gmail.com>
2021-04-06 21:48:57 -0700

    input data stored correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-    console.log(event);
     event.preventDefault();
   
     var listItemEl = document.createElement("li");
